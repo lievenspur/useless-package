e7eb74a7ad3eafc114d03c1f46dd5e9967044ea3
Lieven Spur <lieven.spur@dontturnoffthepower.com>
2023-10-31 18:21:06 +0100

    Testing

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from util.hulu import easy
+
+def main():
+    value: str = input('gimme bites: ')
+    num: int = int(value)
+    print(f"result: {easy(num)}")
+
+if __name__=="__main__":
+    main()
